75ed14cc5ce6b902f72496960c1db6c794c3a06a
loannt <nguyenthimyloan061196@gmail.com>
2017-02-14 14:05:56 +0700

    create action, view submit

---
+++ Controllers/DocumentManagerController.cs
@@ -0,0 +1,37 @@
+﻿using DocShare.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DocShare.Controllers
+{
+    public class DocumentManagerController : Controller
+    {
+        ApplicationDbContext DBContext;
+        public DocumentManagerController()
+        {
+            DBContext = new ApplicationDbContext();
+        }
+        // GET: DocumentManager
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Submit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Submit(TaiLieu model)
+        {
+            model.NgayUpload = DateTime.Now;
+            DBContext.TaiLieus.Add(model);
+            DBContext.SaveChanges();
+            return View();
+        }
+    }
+}
